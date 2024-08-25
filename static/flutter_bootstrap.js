9c595678886220731f5b7f150dac609f67571b40
chel-007 <wstones025@gmail.com>
2024-08-25 20:27:44 +0100

    extra cleanup

--- static/flutter_bootstrap.js
+++ static/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "331553137"
+    serviceWorkerVersion: "3219128993"
   }
 });
